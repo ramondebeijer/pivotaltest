b4e4cffa74e945fc23f6e241a2dff013703d84ee
Rick Ross <rross@pivotal.io>
2016-05-13 09:42:53 -0400

    Initial checkin

---
+++ src/main/java/io/pivotal/demo/HelloWorldController.java
@@ -0,0 +1,34 @@
+package io.pivotal.demo;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HelloWorldController {
+
+	@RequestMapping(method=RequestMethod.GET, value="/")
+	@ResponseBody
+	public String helloWorld()
+	{
+		return "Hello World from Spring Boot. The current time is " + convertCalendarToDateString(Calendar.getInstance());
+	}
+	
+	private static String convertCalendarToDateString(Calendar date)
+	{
+		if (date == null)
+			return null;
+		
+		Date theDate = date.getTime();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+		return sdf.format(theDate);
+	}
+}
+
