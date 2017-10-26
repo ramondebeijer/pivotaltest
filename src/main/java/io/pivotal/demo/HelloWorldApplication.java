3c30fc85b6f4ea5173451069514b621b6cb89cf6
beijerr <ramon.de.beijer@rabobank.nl>
2017-10-26 12:53:40 +0200

    xxx

--- src/main/java/io/pivotal/demo/HelloWorldApplication.java
+++ src/main/java/io/pivotal/demo/HelloWorldApplication.java
@@ -1,12 +1,21 @@
 package io.pivotal.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.actuate.info.SimpleInfoContributor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+
+
+	@Bean
+	public InfoContributor infoContributor() {
+		return new SimpleInfoContributor("Bla", "yek");
 	}
 }
--- src/test/java/io/pivotal/demo/HelloWorldApplicationTests.java
+++ src/test/java/io/pivotal/demo/HelloWorldApplicationTests.java
@@ -2,11 +2,10 @@ package io.pivotal.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = HelloWorldApplication.class)
+//@SpringApplicationConfiguration(classes = HelloWorldApplication.class)
 public class HelloWorldApplicationTests {
 
 	@Test
